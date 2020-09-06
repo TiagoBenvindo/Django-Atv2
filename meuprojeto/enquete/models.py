a772625732dcd067db43f3f45f5fb307b823e485
Tiago Batista <tiagobenvindo111@gmail.com>
2020-09-06 16:04:45 -0300

    implementação do uso de banco

--- meuprojeto/enquete/models.py
+++ meuprojeto/enquete/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Enquete(object):
-    def __init__(self, texto = '', data_publicacao = '', id = 0):
-        self.texto = texto
-        self.data_publicacao = data_publicacao
-        self.id = id
-        
+class Enquete(models.Model):
+    texto = models.CharField(max_length=200)
+    data_publicacao = models.DateField()
+    
 
--- meuprojeto/enquete/views.py
+++ meuprojeto/enquete/views.py
@@ -12,12 +12,13 @@ def bemvindo(request):
 
 
 def enquete(request, enquete_id):
-    enquete = Enquete()
-    if enquete_id == 1:
-        enquete = Enquete('Qual grau de escolaridade?', '24/08/2020')
-    if enquete_id == 2:
-        enquete = Enquete('Qual sua opinião sobre o atual governo?', '25/08/2020')
-    if enquete_id == 3:
-        enquete = Enquete('Qual sua posição sobre aulas presencias no ano de 2020?', '26/08/2020')
+    #enquete = Enquete()
+    enquete = Enquete.objects.get(id=enquete_id)
+    # if enquete_id == 1:
+    #    enquete = Enquete('Qual grau de escolaridade?', '24/08/2020')
+    #if enquete_id == 2:
+    #    enquete = Enquete('Qual sua opinião sobre o atual governo?', '25/08/2020')
+    #if enquete_id == 3:
+    #    enquete = Enquete('Qual sua posição sobre aulas presencias no ano de 2020?', '26/08/2020')
     
     return render(request, 'enquete.html', { "enquete" : enquete})
